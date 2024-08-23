8a5ad43dc3b6f4cfcd52896824bfdea284d83e54
KTvyen <katenguyen08.az@icloud.com>
2024-08-23 08:29:19 -0700

    Delete python/math.py

--- python/math.py
+++
@@ -1,9 +0,0 @@
-#string = "The number of miles we've hiked totals up to {}"
-#miles = 21
-#string2 = string.format(miles)
-#print(string2)
-
-string = "I've eaten {1} pasteries this morning. They had {0} today, I can't belive it!"
-eaten_pasteries = 5
-amt_pasteries = 120
-print(string.format(amt_pasteries, eaten_pasteries))
