9f7d4f9d9237b6af72b3ffd18df06579ad821df2
KTvyen <katenguyen08.az@icloud.com>
2024-08-23 08:27:04 -0700

    exported from Skill Struck

--- python/math.py
+++ python/math.py
@@ -1,33 +1,9 @@
-#pies = 10 
-#shared = pies/3
-#print(round(shared,2))
+#string = "The number of miles we've hiked totals up to {}"
+#miles = 21
+#string2 = string.format(miles)
+#print(string2)
 
-#for x in range(0,5): 
-    #print(x)
-
-#smells = ["Hellow", "helo", "hwlllo", "hllos", "hellos", "hello", "HELWOOS", "HELLO","HellOS", "hwelLS", "lleshe", "hellsoss", "helsoas"]
-#print(smells[0:8:2])
-
-#largest = min(12, 13, 14, 15, 16, 17, 18, 19, 20)
-#print(largest)
-
-#lasers = [3, 10, 4, 15, 11]
-#print(lasers[2] * 10)
-
-#print(round(3.14159265, 3))
-
-#for x in range(3): 
-    #print(x)
-
-
-#hellos = [19, 10, 192, 12837, 182, 1073, 103, 123, 1234, 56, 283, 282]
-#print(hellos[0:10:2])
-#for x in range(0,10,2): 
-    #print(x)
-
-#pizza = 5.879
-
-#print(round(pizza, 2))
-
-#for x in range(10, 21, 1):
-#print(x,2)
+string = "I've eaten {1} pasteries this morning. They had {0} today, I can't belive it!"
+eaten_pasteries = 5
+amt_pasteries = 120
+print(string.format(amt_pasteries, eaten_pasteries))
