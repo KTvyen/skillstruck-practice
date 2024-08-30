b9137f17f8e170c87cc76640f17118e1ffe5a22d
KTvyen <katenguyen08.az@icloud.com>
2024-08-30 09:38:39 -0700

    exported from Skill Struck

---
+++ python/math.py
@@ -0,0 +1,33 @@
+#pies = 10 
+#shared = pies/3
+#print(round(shared,2))
+
+#for x in range(0,5): 
+    #print(x)
+
+#smells = ["Hellow", "helo", "hwlllo", "hllos", "hellos", "hello", "HELWOOS", "HELLO","HellOS", "hwelLS", "lleshe", "hellsoss", "helsoas"]
+#print(smells[0:8:2])
+
+#largest = min(12, 13, 14, 15, 16, 17, 18, 19, 20)
+#print(largest)
+
+#lasers = [3, 10, 4, 15, 11]
+#print(lasers[2] * 10)
+
+#print(round(3.14159265, 3))
+
+#for x in range(3): 
+    #print(x)
+
+
+#hellos = [19, 10, 192, 12837, 182, 1073, 103, 123, 1234, 56, 283, 282]
+#print(hellos[0:10:2])
+#for x in range(0,10,2): 
+    #print(x)
+
+#pizza = 5.879
+
+#print(round(pizza, 2))
+
+for x in range(0, 10, 2):
+print(x)
