3774cd848593d6095ae147c906374e639985898e
KTvyen <katenguyen08.az@icloud.com>
2024-08-30 09:38:46 -0700

    exported from Skill Struck

---
+++ Python 2/removing froma lisst.py
@@ -0,0 +1,33 @@
+#harvest = ["pumpkins", "apples", "corn", "squash", "carrots","squash"]
+#harvest.remove("squash")
+#harvest.pop(1)
+#print(harvest)
+
+#choice = input("What skill do you want?")
+#skills = ["Piano", "Soccer", "Coding", "Cooking", "Writing"]
+#skills.remove(choice)
+#print(skills)
+
+#my_list = input().split()
+#bronze = my_list.pop(2)
+#print(bronze + " got the bronze medal")
+
+#beans = input().split()
+#for x in beans:
+    #if x == "lima":
+        #beans.remove("lima")
+        #print(beans)
+
+#my_list = input().split()
+
+#center = int(len(my_list)/2)
+
+#if len(my_list) % 2 == 1 :
+	#my_list.pop(center)
+#else:
+	#my_list.pop(center)
+	#my_list.pop(center - 1)
+
+#print(my_list)
+
+
