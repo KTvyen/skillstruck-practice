5ac2dedf6ab38507f8576333ee1ffe8be1cf9bad
KTvyen <katenguyen08.az@icloud.com>
2024-08-30 09:38:51 -0700

    exported from Skill Struck

---
+++ Python 2/Adding to and Removing from Dictionaries.py
@@ -0,0 +1,84 @@
+#classmates = { "billy" : 8, "vance" : 15, "alice" : 10}
+#classmates["lily"] = 6
+#print(classmates)
+#adds lily : 6 at the end of the dictionary
+
+#friends = {}
+#friends["lily"] = 6
+#friends["james"] = 10
+#print(friends) 
+#adds lily : 6 and james : 10 to the empty dictionary 
+
+#to remove items make sure to use the pop method 
+# exmple 
+#classmates = { "billy" : 8, "vance" : 15, "alice" : 10}
+#print(classmates)
+#classmates.pop("alice")
+#print(classmates) 
+
+#output > billy : 8 , vance : 15,  alice : 10
+#pt2 > billy : 8, vance : 15
+
+#friends = { "Shane" : 10, "Samantha" : 9, "Shiloh" : 12, "Sean" : 11 }
+
+#friends["Sebastian"] = 8
+#friends.pop("Shiloh")
+#print(friends)
+
+#shape = input("input a shape ")
+#height = input("input a number")
+
+#shapes = shapes = {
+
+#"Triangle": 8,
+
+#"Circle": 15,
+
+#"Square": 10,
+
+#"Rectangle" : 12
+
+#}
+#shapes[shape] = height
+#print(shapes)
+
+#my_shape = input("What shape do you want to add?")
+#my_shape_height = int(input("How tall is your shape?"))
+
+#shapes = {
+    #"Triangle": 8,
+    #"Circle": 15,
+    #"Square": 10,
+	#"Rectangle" : 12,
+#}
+
+#shapes[my_shape] = my_shape_height
+
+#print(shapes)
+
+#choice = input("Which kind of tree do you want to remove from the dictionary? ").lower()
+
+#trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
+
+#trees.pop(choice)
+#print(trees)
+##choice = input("which goal do you want to remove?" ).lower()
+#goals = { "piano" : 5, "run" : 3, "paint" : 2, "serve" : 7, "homework" : 7}
+
+#goals.pop(choice)
+#print(goals)
+
+#new_dino = input("What dino do you want to add?")
+#dino_height = int(input("How tall is your dinosaur?"))
+#dinosaur = {
+#}
+
+#dinosaur[new_dino] = dino_height
+
+#while new_dino != "triceratops":
+	#new_dino = input("What dino do you want to add?")
+	#dino_height = int(input("How tall is your dinosaur?"))
+	#dinosaur[new_dino] = dino_height
+
+#print(dinosaur)
+
