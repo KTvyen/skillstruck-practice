a483ccc64f2936617c27d27f7448f5730376a2f5
KTvyen <katenguyen08.az@icloud.com>
2024-08-30 09:38:41 -0700

    exported from Skill Struck

---
+++ python/twilights list (MLP ref).py
@@ -0,0 +1,69 @@
+#List: a data group that is ordered (numbered) and changeable (you can change the list items).  Allows duplicates.
+
+#Tuple : A group of data that cannot be changed. Tuples are created with ( ) parentheses.: a data group that is unordered and unchangeable.  Allows duplicates.
+
+#Dictionary: a data group that is unordered, changeable and indexed.  No duplicates.
+
+#friend = ["kate", "herself", "me"]
+#print(friend)
+
+#distance = [5, 7, 10, 18, 20]
+#print(distance)
+
+#informtion = [16, "kate", 20, 18]
+#print(informtion)
+
+#candy = ["chocolate", "gummies", "candycanes", "peppermint bark"] 
+#pieces = [20, 20, 6, 300]
+
+#print(candy)
+#print(pieces)
+
+#smells = [3, 14, 5 ,55, 8, 8, 9]
+#smells.sort()
+#print(smells)
+
+#things = ["bottle", "keys", "lunchbox", "chair", "couch", "pens", "flowers"]
+#print(things[0:3])
+#things[6] = "socks"
+#print(things)
+#print(len(things))
+
+#flowers = ["rose", "tulip", "daffodil", "dandelion", "sunflower", "lily", "lotus"]
+
+#print(flowers[0:6:3])
+
+#flowers = ["rose", "tulip", "daffodil", "dandelion", "sunflower", "lily", "lotus"]
+
+#flowers.sort()
+#print(flowers)
+
+#my_list = [int(n) for n in input().split()]
+
+#current = my_list[0]
+
+#for i in range(1, len(my_list)):
+    #if my_list[i] > current:
+        #print(my_list[i], end=' ')
+    #current = my_list[i]
+
+#my_list = [int(n) for n in input().split()]
+
+# Initialize an empty list to store unique elements
+#new_list = []
+
+# Iterate through the input list
+#for x in my_list:
+    # If x is not already in new_list, add it
+    #if x not in new_list:
+        #new_list.append(x)
+
+# Print the number of unique elements
+#print(len(new_list))
+
+#my_list = [int(n) for n in input().split()]
+
+#for i in range(0, len(my_list) -1 ,2 ):
+	#my_list[i], my_list[i+1] = my_list[i+1], my_list[i]
+#print(my_list)
+
