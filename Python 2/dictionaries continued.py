4403abaeefba61cb6432abe979fa2a50cc8075a8
KTvyen <katenguyen08.az@icloud.com>
2024-08-30 09:38:52 -0700

    exported from Skill Struck

---
+++ Python 2/dictionaries continued.py
@@ -0,0 +1,56 @@
+#when counta=ing the items in a list (like pylist) the counting length stars at 1
+
+#example of this 
+#classmates = {"Hello" : 0, "Jleo" : 7, "kal" : 9, "welxcome" :10}
+#print(len(classmates))
+#will print 4 becasue thats the number of items in the list if its starts countaing at 1
+
+#you can also check for a key in a vaule (reminer (key: value))
+
+
+#dino = {"t rex" : 50, "long neck" : 100, "raptor" : 15, "triceratops"  : 30}
+
+#if "t rex" in dino: 
+    #print("t rex is in youe dictionary")
+
+#checks for t rex and will only print that staement if it is in the dictionary
+
+#you can also he opposite 
+
+
+#dino = {"t rex" : 50, "long neck" : 100, "raptor" : 15, "triceratops"  : 30}
+
+#if "rhino" not in dino: 
+    #print("rhino is not in your dictionary")
+
+#will print rhino is not in your dictinary 
+
+
+#you can convert a py list to a py dictionary 
+
+#example 
+
+#garden = ["pumkin", "squash",  "corn", "tomatoes"] 
+#garden_dictionary = dict.fromkeys(garden, "Harvested")
+#break down the code (set a dictionary ) with dict, name the list (garden) use fromkeys uses the list to set a vaule to all  the keys (garden, "vaule") in he second place is the vaule that will be set to all keys in the dictionary 
+#print(garden_dictionary)
+
+#coins = {"pennies" : .01, "nickels" : .05 , "dimes" : .1, "quarters" : .25}
+#coins["silvar dollar"] = 1
+#coins.pop("pennies")
+#print(coins)
+#print(len(coins))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
