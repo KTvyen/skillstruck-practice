4b6ce42bf132132c9d73addbd7d26484cd43674a
KTvyen <katenguyen08.az@icloud.com>
2024-08-30 09:38:48 -0700

    exported from Skill Struck

---
+++ Python 2/funtions.py
@@ -0,0 +1,74 @@
+#funtions is a code that only runs when its called (like a button)
+
+#def weather():
+    #print("it's a soggy day outside!")
+    #out was nothing 
+
+#to press the button (funtion) "weather" you have to call on it 
+#press = call 
+
+#weather()
+#untill funtion was called here did the string  print 
+
+#the use is that we have this code written but don't want to use it right away
+
+# it can be compared to an alarm too 
+#alarms exist but won't ring unless you set it up
+
+#local variable is a inside of a funtion (that means it will only be called when the funtion is called 
+
+#favorite = "I love juice" #global variable bcs outside function
+#def myfunction():
+    #global fruit #local variable cause inside function
+    #fruit = "apple" 
+    
+#myfunction()
+#print(favorite)
+#print(fruit)
+
+#def race():
+    #print("Darius won the race!")
+
+#race()
+
+#def charlie():
+    #print("Charlie is the best dog in the whole world!")
+
+#charlie()
+
+#start = input("Do you want to luanch game? Enter yes or no ").lower()
+
+#def start_game():
+    #if start == "yes":
+        #print("Initialization Complete")
+    #else:
+        #print("Initialization Failed")
+
+#start_game()
+
+#chickens = 20 
+#eggs_per_week = 2
+
+#def farmer():
+    #print(chickens*eggs_per_week)
+#farmer()
+
+#number = int(input("A number please "))
+#def numBOT():
+    #print("Hello" * number)
+#numBOT()
+
+#def fruit():
+    #question = input("Name a fruit ")
+    #print(question*3)
+
+#fruit()
+
+length = 7
+def surface_area():
+    print(length*length*6)
+surface_area()
+
+def volume():
+    print(length*length*length)
+volume()
